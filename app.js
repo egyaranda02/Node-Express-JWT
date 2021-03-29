76c6b7798430dbf02c7eabff85f256bef0a3caea
egyaranda02 <egyaranda02@gmail.com>
2021-03-29 08:16:50 +0700

    Sucess creating JWT when register

--- app.js
+++ app.js
@@ -29,17 +29,3 @@ app.listen(3000, ()=>{
 app.get('/', (req, res) => res.render('home'));
 app.get('/smoothies', (req, res) => res.render('smoothies'));
 app.use(authRoutes);
-
-app.get('/set-cookies', (req, res)=>{
-  res.cookie('newUser', false);
-  res.cookie('isCoder', true, {maxAge: 1000 * 60 * 60 * 24, httpOnly: true});
-
-  res.send('you got the cookie!');
-});
-
-app.get('/read-cookies', (req, res)=>{
-  const cookies = req.cookies;
-  console.log(cookies);
-
-  res.json(cookies);
-});
--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,16 @@
 var connection = require('../connection');
 var mysql = require('mysql');
+var jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
+const saltRounds = 10;
 const { isEmail } = require('validator');
+const maxAge = 60 * 60 * 5;
+
+const createToken = (id) =>{
+    return jwt.sign({id}, 'This is a secret key', {
+        expiresIn: maxAge
+    });
+}
 
 module.exports.signup_get = (req, res)=>{
     res.render('signup');
@@ -11,41 +20,43 @@ module.exports.login_get = (req, res)=>{
     res.render('login');
 }
 
-module.exports.signup_post = async function(req, res){
-    const password = req.body.password;
-    const encryptedPassword = await bcrypt.hash(password, 10);
-    
+module.exports.signup_post = function(req, res){
     var post = {
         email: req.body.email,
-        password: encryptedPassword
+        password: req.body.password
     }
-    if(isEmail(post.email)){
-        var query = "SELECT email FROM ?? WHERE ?? = ?";
-        var table = ["users", "email", post.email];
-        query = mysql.format(query, table);
-        connection.query(query, function(error, rows){
-            if(error){
-                console.log(error);
-            }else{
-                if(rows.length == 0 ){
-                    var query = "INSERT INTO ?? SET ?";
-                    var table = ["users"];
-                    query = mysql.format(query, table);
-                    connection.query(query, post, function(error, rows){
-                        if(error){
-                            console.log(error);
-                        }else{
-                            res.send("New user created!");
-                        }
-                    })
+    bcrypt.hash(post.password, saltRounds, function(err, hash) {
+        if(isEmail(post.email)){
+            post.password = hash;
+            var query = "SELECT email FROM ?? WHERE ?? = ?";
+            var table = ["users", "email", post.email];
+            query = mysql.format(query, table);
+            connection.query(query, function(error, rows){
+                if(error){
+                    console.log(error);
                 }else{
-                    res.send("Email already used by another user");
+                    if(rows.length == 0 ){
+                        var query = "INSERT INTO ?? SET ?";
+                        var table = ["users"];
+                        query = mysql.format(query, table);
+                        connection.query(query, post, function(error, rows){
+                            if(error){
+                                console.log(error);
+                            }else{
+                                const token = createToken(rows.insertId);
+                                res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000});
+                                res.json({ user: rows.insertId });
+                            }
+                        });
+                    }else{
+                        res.send("Email already used by another user");
+                    }
                 }
-            }
-        })
-    }else{
-        res.send('Please enter a valid email!');
-    }
+            })
+        }else{
+            res.send('Please enter a valid email!');
+        }
+    });
 }
 
 module.exports.login_post = (req, res)=>{
